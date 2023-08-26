f54032a242b4ac81499286ceb5651c9f8817b905
solendil <matthieu.dumas@gmail.com>
2023-08-26 15:31:46 +0200

    componentisation headers & footers

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
+// this function executes when the document is ready
+$(function () {
+  // load header and footer
+  $("#component-header").load("components/header.html");
+  $("#component-footer").load("components/footer.html");
 
-const filterContainer = document.querySelector(".gallery-filter"),
-galleryItems = document.querySelectorAll(".gallery-item");
+  // handle filters
+  const filterContainer = document.querySelector(".gallery-filter"),
+    galleryItems = document.querySelectorAll(".gallery-item");
 
-filterContainer.addEventListener("click", (event) =>{
-  if(event.target.classList.contains("filter-item")){
-       // deactivate existing active 'filter-item'
-       filterContainer.querySelector(".active").classList.remove("active");
-       // activate new 'filter-item'
-       event.target.classList.add("active");
-       const filterValue = event.target.getAttribute("data-filter");
-       galleryItems.forEach((item) =>{
-      if(item.classList.contains(filterValue) || filterValue === 'all'){
+  filterContainer.addEventListener("click", (event) => {
+    if (event.target.classList.contains("filter-item")) {
+      // deactivate existing active 'filter-item'
+      filterContainer.querySelector(".active").classList.remove("active");
+      // activate new 'filter-item'
+      event.target.classList.add("active");
+      const filterValue = event.target.getAttribute("data-filter");
+      galleryItems.forEach((item) => {
+        if (item.classList.contains(filterValue) || filterValue === "all") {
           item.classList.remove("hide");
-           item.classList.add("show");
-      }
-      else{
+          item.classList.add("show");
+        } else {
           item.classList.remove("show");
           item.classList.add("hide");
-      }
-       });
-  }
+        }
+      });
+    }
+  });
 });
